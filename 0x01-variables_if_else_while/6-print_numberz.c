684d8645cbec299225fa651f68e7542401a2499d
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-02-17 18:15:33 -0800

    variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,19 +4,18 @@
 
 /**
  * main - Entry point
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 
 {
 
-	int i;
+	int d;
 
-	for (i = 0; i < 10; i++)
-	       printf("%d", i);
-printf("\n");
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
 
-return (0);
-
+	return (0);
 }
