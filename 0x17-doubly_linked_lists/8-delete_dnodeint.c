1b5713f5009c8bde2bd83c9b32a79eec62d5fd8b
Abdulhafiz108 <Ahgazali108@gmail.com>
2023-06-15 23:55:35 +0000

    doubly_linked_lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,17 +4,17 @@
  * delete_dnodeint_at_index - Deletes the node at and index of a doubly list
  * @head: The head node of a doubly list.
  * @index: The position of node to delete.
+ * Return: 1 on success and -1 otherwise.
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	int len = 0;
+	unsigned int len = 0;
 	dlistint_t *tmp;
 
 	tmp = *head;
 	if (*head == NULL)
 		return (-1);
-
 	while (tmp)
 	{
 		tmp = tmp->next;
@@ -22,19 +22,38 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if (index > len - 1)
 		return (-1);
-
 	tmp = *head;
-	if (index == 0)
-	{
-		tmp = tmp->next;
-		tmp->prev = NULL;
-		free(*head);
-		*head = tmp;
-	}
 	len = 0;
 	while (len != index)
 	{
 		tmp = tmp->next;
 		len++;
 	}
+	if ((index == 0))
+	{
+		if (tmp->next == NULL)
+		{
+			free(tmp);
+			*head = NULL;
+		}
+		else
+		{
+			tmp = (*head)->next;
+			tmp->prev = NULL;
+			free(*head);
+			*head = tmp;
+		}
+	}
+	else if (tmp->next == NULL)
+	{
+		tmp->prev->next = NULL;
+		free(tmp);
+	}
+	else
+	{
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		free(tmp);
+	}
+	return (1);
 }
