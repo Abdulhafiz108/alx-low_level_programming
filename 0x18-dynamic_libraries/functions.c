e33f456452b696f71f972dc27ab38ab032fc9c9f
Abdulhafiz108 <Ahgazali108@gmail.com>
2023-06-19 11:34:08 +0000

    dynamic_libraries

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,61 @@
+#include "functions.h"
+
+/**
+ * add - Adds two numbers.
+ * @a: First number.
+ * @b: Second number.
+ * Return: The sum of the two numbers.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts one number from another.
+ * @a: First number.
+ * @b: Second number.
+ * Return: The difference between the two numbers.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: First number.
+ * @b: Second number.
+ * Return: The multiplied value of the two numbers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides a number by another
+ * @a: First number.
+ * @b: Second number.
+ * Return: The value of the division.
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Calculates the modulus value of two numbers
+ * @a: First number.
+ * @b: Second number.
+ * Return: The value of the modulus operation.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
