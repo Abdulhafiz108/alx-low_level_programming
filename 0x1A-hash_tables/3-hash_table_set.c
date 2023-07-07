75fce171219d99e5ee6e066a8db5f381d4f8ecc2
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-07-07 12:37:06 +0000

    hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,23 +12,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new;
+	hash_node_t *start;
 
 	if (!key)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	new->key =(char *)key;
 	new->value = strdup(value);
 	new->next = NULL;
-	if (ht->array[index] == NULL)
-		ht->array[index] = new;
-	else
+	start = ht->array[index];
+	if (start != NULL)
 	{
+		while (start)
+		{
+			if (strcmp(start->key, key) == 0)
+			{
+				free(start->value);
+				start->value = strdup(value);
+				if (start->value == NULL)
+					return (0);
+				free(new->value);
+				free(new);
+				return (1);
+			}
+			start = start->next;
+		}
 		new->next = ht->array[index];
 		ht->array[index] = new;
+		return (1);
 	}
+	ht->array[index] = new;
 	return (1);
 }
