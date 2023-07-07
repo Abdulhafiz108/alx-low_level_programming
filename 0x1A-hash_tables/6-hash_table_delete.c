ba1491387d041392dfa77c15cbeabab53755fa42
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-07-07 14:28:51 +0000

    hash_tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *new;
+
+	if (ht != NULL)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			new = ht->array[index];
+			while (new != NULL)
+			{
+				ht->array[index] = ht->array[index]->next;
+				free(new->key);
+				free(new->value);
+				free(new);
+				new = ht->array[index];
+			}
+		}
+	}
+}
