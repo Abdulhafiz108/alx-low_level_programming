dea491cb0098707f9016f18ef735f348c1dc19ca
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-07-07 10:43:00 +0000

    hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
 	hash_table_t *new;
 
 	new = malloc(sizeof(hash_table_t));
-	new->size = size;
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-	new->array = malloc(sizeof(hash_node_t) * size);
+
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (new->array == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
 		new->array[i] = NULL;
-		i++;
 	}
 	return (new);
 }
